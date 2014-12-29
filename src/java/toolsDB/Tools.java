d17d8ed29cbf3c2170685af729e76d43aacb821a
Max <marlier.maxime@gmail.com>
2014-12-29 22:19:56 +0100

    [add] verifyLogin(...)

--- src/java/toolsDB/Tools.java
+++ src/java/toolsDB/Tools.java
@@ -5,10 +5,33 @@
  */
 package toolsDB;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author max
  */
 public class Tools {
     
+    public static boolean verifyLogin( Connection con, String login, 
+                                       String passwd){
+        Statement stmt=null;
+        try {
+            stmt = con.createStatement();
+            login="'"+login+"'";
+            passwd="'"+passwd+"'";
+            ResultSet rs=stmt.executeQuery("Select * "
+                     + "from User where login="+login
+                     +" and passwd="+passwd);
+            boolean ok=rs.next();
+            System.out.println(ok);
+            if(ok)  return true;
+
+        } catch (SQLException ex) {ex.printStackTrace(); }
+
+      return false;
+    }
 }
