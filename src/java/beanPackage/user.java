f82c1e0ec7adb642b0be9474eded3efd57415117
Max <marlier.maxime@gmail.com>
2014-12-30 17:33:01 +0100

    [add] bean data data for user

--- src/java/beanPackage/user.java
+++ src/java/beanPackage/user.java
@@ -13,6 +13,30 @@ public class user {
     
     private String name, first_name, login, passwd;
 
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+
     public user() {};
     
 
