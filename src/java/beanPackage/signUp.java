6e4dbec4d43fc4a0fd0ed7a3ebfbbfdc52fc00c4
Max <marlier.maxime@gmail.com>
2015-01-02 17:21:15 +0100

    sign up view

---
+++ src/java/beanPackage/signUp.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beanPackage;
+
+/**
+ *
+ * @author max
+ */
+public class signUp {
+    
+    private String name, firstName, pseudo, passwd, cp;
+
+    /**
+     * Creates a new instance of signUp
+     */
+    public signUp() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getPseudo() {
+        return pseudo;
+    }
+
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+
+    public String getCp() {
+        return cp;
+    }
+
+    public void setCp(String cp) {
+        this.cp = cp;
+    }
+    
+    
+}
